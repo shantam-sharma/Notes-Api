9db7a9fc282778a4a32f3e03c8e487b50cea877e
shantam-sharma <writetoshantam@gmail.com>
2026-05-19 22:47:21 +0530

    initialize middleware

--- internal/middleware/auth_middleware.go
+++ internal/middleware/auth_middleware.go
@@ -1,1 +1,10 @@
 package middleware
+
+import "net/http"
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		next.ServeHTTP(w, r)
+	})
+}
